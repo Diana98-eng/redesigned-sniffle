b4d1f4627dc338ca797ded619d9fa94294cc0bed
diana <d1234@outlook.es>
2025-05-08 09:44:17 -0500

    primer commit

---
+++ src/main/java/com/politenico/programaciondos/ProgramaciondosApplication.java
@@ -0,0 +1,13 @@
+package com.politenico.programaciondos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramaciondosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaciondosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/politenico/programaciondos/ProgramaciondosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politenico.programaciondos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramaciondosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
