7fcb6fe419f2cd4023225c4445b0c711b75c1767
diana <d1234@outlook.es>
2025-05-20 10:05:33 -0500

    cuarto comentario

---
+++ src/main/java/com/politenico/programaciondos/models/Usuario.java
@@ -0,0 +1,13 @@
+package com.politenico.programaciondos.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entit
+public class Usuario {
+
+}
